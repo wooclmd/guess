72c7db9a78b1ef8779c6eca0f1aed8489d7eb067
yvaine <wooclmd@gmail.com>
2020-02-25 07:29:07 +0800

    second commit

--- guess.py
+++ guess.py
@@ -1,13 +1,21 @@
 import random
+start = input('請輸入開始值: ')
+end = input('請輸入結束值: ')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
+r = random.randint(start, end)
+count = 0
 while True:
+	count = count + 1 
 	num = input('請猜數字: ')
 	num = int(num)
 	if num == r:
-		print('答對了!')
+		print('猜對了')
+		print('這是你猜的第', count, '次')
 		break
 	elif num > r:
-		print('答錯了,比', num, '小')
+		print('猜錯了, 比', num, '小')
 	else:
-		print('答錯了,比', num, '大')
+		print('猜錯了, 比', num, '大')
+	print('這是你猜的第', count, '次')
